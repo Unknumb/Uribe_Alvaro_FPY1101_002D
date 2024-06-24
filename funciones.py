5c0af068e99e12b6ba2085f327afe258e1191ee1
juanignaciotoled <ju.toledod@duocuc.cl>
2024-06-24 09:07:08 -0400

    iniciandomenu

---
+++ funciones.py
@@ -0,0 +1,6 @@
+
+matrizLibros=[]
+
+
+def agregarLibro():
+    print("Hola")
---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+print("\n1. Agregar un libro")
+print("2. Ver todos los libros")
+print("3. Modificar un libro")
+print("4. Eliminar un libro.")
+print("5. Guardar colección en un archivo")
+print("6. Salir del programa")
